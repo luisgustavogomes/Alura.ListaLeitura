32baa42c0e5e05ae2f6b8202c6202926bf7ee532
luisgustavogomes <luisgustavogomes@outlook.com>
2019-02-08 21:36:33 -0200

    Adicionar arquivos de projeto.

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void ConfigureProduction()
+        {
+
+        }
+
+        public void LivrosParaLer()
+        {
+            var _repo = new LivroRepositorioCSV();
+            _repo.ParaLer.ToString();
+
+        }
+    }
+}
