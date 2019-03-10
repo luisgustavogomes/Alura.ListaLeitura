29bbde9b3679982c17823ebf6f8d599e1b281793
luisgustavogomes <luisgustavogomes@outlook.com>
2019-03-10 20:25:52 -0300

    Route

--- Alura.ListaLeitura.App/Negocio/ListaDeLeitura.cs
+++ Alura.ListaLeitura.App/Negocio/ListaDeLeitura.cs
@@ -7,18 +7,14 @@ namespace Alura.ListaLeitura.App.Negocio
     public class ListaDeLeitura
     {
         private List<Livro> _livros;
+        public string Titulo { get; set; }
+        public IEnumerable<Livro> Livros { get { return _livros; } }
 
         public ListaDeLeitura(string titulo, params Livro[] livros)
         {
             Titulo = titulo;
             _livros = livros.ToList();
             _livros.ForEach(l => l.Lista = this);
-        }
-
-        public string Titulo { get; set; }
-        public IEnumerable<Livro> Livros
-        {
-            get { return _livros; }
         }
 
         public override string ToString()
--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,19 +1,84 @@
-﻿using Alura.ListaLeitura.App.Repositorio;
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
-        public void ConfigureProduction()
+        public LivroRepositorioCSV _repo { get; private set; }
+
+        public Startup() => _repo = new LivroRepositorioCSV();
+
+        public void Configure(IApplicationBuilder app)
         {
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/ParaLer", LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", LivrosLendo);
+            builder.MapRoute("Livros/Lidos", LivrosLidos);
+            builder.MapRoute("Cadastro/NovoLivro/{Nome}/{Autor}", NovoLivroParaLer);
+            var rotas = builder.Build();
+            app.UseRouter(rotas);
+            //app.Run(builder.);
+        }
+
+        public Task NovoLivroParaLer(HttpContext context)
+        {
+            var livro = new Livro()
+            {
+                Titulo = context.GetRouteValue("Nome").ToString(),
+                Autor = context.GetRouteValue("Autor").ToString()
+            };
+            _repo.Incluir(livro);
+            return context.Response.WriteAsync("O Livro foi adicionado com sucesso");
+        }
+
+        public void ConfigureServices(IServiceCollection service)
+        {
+            service.AddRouting();
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
+            {
+                { "/Livros/ParaLer", LivrosParaLer},
+                { "/Livros/Lendo", LivrosLendo},
+                { "/Livros/Lidos", LivrosLidos},
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                var metodo = caminhosAtendidos[context.Request.Path];
+                return metodo.Invoke(context);
+            }
+
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Caminho inexistente!!!");
 
         }
 
-        public void LivrosParaLer()
+        public Task LivrosParaLer(HttpContext context)
         {
-            var _repo = new LivroRepositorioCSV();
-            _repo.ParaLer.ToString();
-
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
         }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
+        }
+
+
     }
 }
