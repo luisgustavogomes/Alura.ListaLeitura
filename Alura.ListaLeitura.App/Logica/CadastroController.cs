969514b707af620dafbbcdacb3b93d67f04769c8
luis gomes <luisgustavogomes@outlook.com>
2019-03-27 12:54:41 -0300

    Alterações nos métodos que anteriormente trabalhavam com o requestDelegate

---
+++ Alura.ListaLeitura.App/Logica/CadastroController.cs
@@ -0,0 +1,39 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class CadastroController
+    {
+
+        public string Incluir(Livro livro)
+        {
+            var _repo = new LivroRepositorioCSV();
+            _repo.Incluir(livro);
+            return "O Livro foi adicionado com sucesso";
+        }
+
+        public static Task ExibeFormulario(HttpContext context)
+        {
+            return context.Response.WriteAsync(HtmlUtils.CarregaArquivoHtml("Formulario"));
+        }
+
+        
+        //public static Task IncluirExemploSemCoreMvc(HttpContext context)
+        //{
+        //    var livro = new Livro()
+        //    {
+        //        Titulo = context.Request.Form["tnome"].First(),
+        //        Autor = context.Request.Form["tautor"].First()
+        //    };
+        //    var _repo = new LivroRepositorioCSV();
+        //    _repo.Incluir(livro);
+        //    return context.Response.WriteAsync("O Livro foi adicionado com sucesso");
+        //}
+
+    }
+}
---
+++ Alura.ListaLeitura.App/Logica/LivrosController.cs
@@ -0,0 +1,60 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class LivrosController
+    {
+
+        private static string CarregaLista(IEnumerable<Livro> livros)
+        {
+            var conteudoHtml = HtmlUtils.CarregaArquivoHtml("Lista");
+            foreach (var livro in livros)
+            {
+                conteudoHtml = conteudoHtml.Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
+            }
+            return conteudoHtml.Replace("#NOVO-ITEM#", "");
+        }
+                       
+        public string ParaLer() => CarregaLista(new LivroRepositorioCSV().ParaLer.Livros);
+
+        public string Lendo() => CarregaLista(new LivroRepositorioCSV().Lendo.Livros);
+
+        public string Lidos() => CarregaLista(new LivroRepositorioCSV().Lidos.Livros);
+
+        /// <summary>
+        ///  Exemplo de metodo utilizando AspNetCoreMvc (Não necessita de retorno Task, nem o HttpContext como parametro)
+        /// </summary>
+        /// <returns></returns>
+        public string Teste() => "Nova funcionalidade implementada";
+
+
+        public string Detalhes(int id)
+        {
+            return new LivroRepositorioCSV().Todos.First(l => l.Id == id).Detalhes();
+        }
+
+
+
+
+        //--------------------------------------------------------------------------//
+
+
+        /// <summary>
+        /// Exemplo de criação de metodo utilizando requestDelegate 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        //public static Task Teste(HttpContext context)
+        //{
+        //    return context.Response.WriteAsync("Nova funcionalidade implementada");
+        //}
+
+    }
+}
